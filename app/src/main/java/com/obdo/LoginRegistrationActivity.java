b0538daddadd43d61029c1970ad13a00feebc6ec
Ivsucram <ivsucram@gmail.com>
2014-12-10 23:03:04 +0800

    Creation of screens and controllers necessary for the user registration/login

---
+++ app/src/main/java/com/obdo/LoginRegistrationActivity.java
@@ -0,0 +1,9 @@
+package com.obdo;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Marcus Vinícius de Carvalho on 12/10/2014.
+ */
+public class LoginRegistrationActivity extends ActionBarActivity {
+}
---
+++ app/src/main/java/com/obdo/SessionController.java
@@ -0,0 +1,7 @@
+package com.obdo;
+
+/**
+ * Created by Marcus Vinícius de Carvalho on 12/10/2014.
+ */
+public class SessionController {
+}
