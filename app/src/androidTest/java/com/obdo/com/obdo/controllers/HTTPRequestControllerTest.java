7cd3dda838ee4f94c53c5a11bcaf2e01ceb05f8d
Ivsucram <ivsucram@gmail.com>
2014-12-13 14:15:24 +0800

    Fixing some packages - part 2

--- app/src/androidTest/java/com/obdo/com/obdo/controllers/HTTPRequestControllerTest.java
+++
@@ -1,8 +0,0 @@
-package com.obdo.com.obdo.controllers;
-
-import android.test.InstrumentationTestCase;
-
-/**
- * Created by Marcus Vinícius de Carvalho on 12/11/2014.
- */
-public class HTTPRequestControllerTest extends InstrumentationTestCase {}
