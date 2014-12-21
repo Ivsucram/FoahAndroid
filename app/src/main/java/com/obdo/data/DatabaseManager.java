ae72b5b46a09b260b1f5b20ff61080ef15760b0d
Ivsucram <ivsucram@gmail.com>
2014-12-22 06:38:13 +0800

    Comments for class DatabaseManager

--- app/src/main/java/com/obdo/data/DatabaseManager.java
+++ app/src/main/java/com/obdo/data/DatabaseManager.java
@@ -6,11 +6,27 @@ import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 
 /**
- * Created by Ivsucram on 12/20/2014.
+ * Manage connection with the database
+ * @param <H> SQLite database open helper. Manage when the application needs to create or upgrade its database.
+ * @author Marcus Vinícius de Carvalho
+ * @since 12/22/2014
+ * @version 1.0
+ * @see com.obdo.data.DatabaseHelper
  */
 public class DatabaseManager<H extends OrmLiteSqliteOpenHelper> {
+    /**
+     * SQLite database open helper. Manage when the application needs to create or upgrade its database.
+     * @since 12/22/2014
+     * @see com.obdo.data.DatabaseHelper
+     */
     private H helper;
 
+    /**
+     * Create and return an instance of the SQLite (There can be only one connection with the SQLite)
+     * @param context Context of the activity that calls it
+     * @return SQLite database open helper. Manage when the application needs to create or upgrade its database.
+     * @see com.obdo.data.DatabaseHelper
+     */
     public H getHelper(Context context) {
         if(helper == null) {
             helper = (H) OpenHelperManager.getHelper(context, DatabaseHelper.class);
@@ -18,6 +34,11 @@ public class DatabaseManager<H extends OrmLiteSqliteOpenHelper> {
         return helper;
     }
 
+    /**
+     * Close connection with the database (There can be only one connection with the SQLite)
+     * @param helper SQLite database open helper. Manage when the application needs to create or upgrade its database.
+     * @see com.obdo.data.DatabaseHelper
+     */
     public void releaseHelper(H helper) {
         if (helper != null) {
             OpenHelperManager.releaseHelper();
